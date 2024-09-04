e814d31f324f386a9e32246b9749e6cddbb70664
Tin Vo <ntinvo@gmail.com>
2024-09-04 10:52:24 -0400

    updated build and config - 2

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 # add your customization script here.
 
 cd /opt/ssfs/runtime/container-scripts/imagebuild
-./generateImages.sh --REPO=${REPO} --MODE=base,agent --WAR_FILES=smcfs --EXPORT=false
+./generateImages.sh --REPO="${OUTPUT_REGISTRY}/ntinvo" --MODE=base,agent --WAR_FILES=smcfs --EXPORT=false
 
 # Add authentication to access the Red Hat OpenShift Container Platform Docker registry. 
 # Tag and push the images to Red Hat OpenShift Container Platform Docker registry.
 
 (echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
-buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}
+buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/oms-base:latest
+buildah push --tls-verify=false --authfile=/tmp/.dockercfg ${OUTPUT_REGISTRY}/oms-agent:latest
