9398dfb55e1397f69b90cbace73b7635d8f0ea6f
Tin Vo <ntinvo@gmail.com>
2024-09-23 11:03:29 -0400

    trying test

---
+++ test/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# add your customization script here.
+sudo -u omsuser /bin/bash << 'EOF'
+
+export REPO=${REPO:-"localhost"}
+export MODE=${MODE:-"app"}
+export WAR_FILES=${WAR_FILES:-"smcfs"}
+
+# Go to the directory where the script is located
+cd /opt/ssfs/runtime/container-scripts/imagebuild
+
+# Generate the images
+./generateImages.sh --REPO=$REPO --MODE=$MODE --WAR_FILES=$WAR_FILES --EXPORT=false
+
+# Push the images to the registry
+(echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
+for i in $(echo $MODE | tr "," "\n"); do
+  echo "Pushing image $REPO/om-$i:10.0"
+  buildah push --tls-verify=false --authfile=/tmp/.dockercfg $REPO/om-$i:10.0
+done
+
+EOF
