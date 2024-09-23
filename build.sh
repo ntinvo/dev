e08281adcf34cda0b879202e4af4d38221205361
Tin Vo <ntinvo@gmail.com>
2024-09-23 14:53:50 -0400

    changed build script - 1

--- build.sh
+++ build.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
 # add your customization script here.
-sudo -iu omsuser bash -c "
+
+sudo -u omsuser /bin/bash << 'EOF'
+
+source ~/.bashrc
 whoami
-export REPO=\${REPO:-'localhost'}
-export MODE=\${MODE:-'app'}
-export WAR_FILES=\${WAR_FILES:-'smcfs'}
 
+export REPO=${REPO:-"localhost"}
+export MODE=${MODE:-"app"}
+export WAR_FILES=${WAR_FILES:-"smcfs"}
+
+# Go to the directory where the script is located
 cd /opt/ssfs/runtime/container-scripts/imagebuild
-./generateImages.sh --REPO=\$REPO --MODE=\$MODE --WAR_FILES=\$WAR_FILES --EXPORT=false
 
-(echo '{ \"auths\": ' ; sudo cat \$PUSH_DOCKERCFG_PATH/.dockercfg ; echo '}') > /tmp/.dockercfg
-for i in \$(echo \$MODE | tr ',' '\n'); do
-  echo 'Pushing image \$REPO/om-\$i:10.0'
-  buildah push --tls-verify=false --authfile=/tmp/.dockercfg \$REPO/om-\$i:10.0
+# Generate the images
+./generateImages.sh --REPO=$REPO --MODE=$MODE --WAR_FILES=$WAR_FILES --EXPORT=false
+
+# Push the images to the registry
+(echo "{ \"auths\": " ; sudo cat $PUSH_DOCKERCFG_PATH/.dockercfg ; echo "}") > /tmp/.dockercfg
+for i in $(echo $MODE | tr "," "\n"); do
+  echo "Pushing image $REPO/om-$i:10.0"
+  buildah push --tls-verify=false --authfile=/tmp/.dockercfg $REPO/om-$i:10.0
 done
-"
+
+EOF
